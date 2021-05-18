7b59975605f6e5e6a029347bdcbd0775976fa6fb
jcorrl00 <jcorrl00@estudiantes.unileon.es>
2021-05-18 11:50:26 +0200

    Registros de las clases
    Creación de los xhtml para que un admin pueda registrar objetos de todas las clases en la base de datos

--- src/main/java/com/unileon/controller/ActorController.java
+++ src/main/java/com/unileon/controller/ActorController.java
@@ -8,6 +8,7 @@ package com.unileon.controller;
 import com.unileon.EJB.ActorFacadeLocal;
 import com.unileon.modelo.Actor;
 import java.io.Serializable;
+import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
@@ -36,4 +37,51 @@ public class ActorController implements Serializable{
         } catch (Exception e) {
         }
     } 
+
+    public Actor getActor() {
+        return actor;
+    }
+
+    public void setActor(Actor actor) {
+        this.actor = actor;
+    }
+
+    public ActorFacadeLocal getActorEJB() {
+        return actorEJB;
+    }
+
+    public void setActorEJB(ActorFacadeLocal actorEJB) {
+        this.actorEJB = actorEJB;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 47 * hash + Objects.hashCode(this.actor);
+        hash = 47 * hash + Objects.hashCode(this.actorEJB);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ActorController other = (ActorController) obj;
+        if (!Objects.equals(this.actor, other.actor)) {
+            return false;
+        }
+        if (!Objects.equals(this.actorEJB, other.actorEJB)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
 }
